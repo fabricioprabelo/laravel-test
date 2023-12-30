b68677feaa8fb64325bc1ee6574d0f2336baa3ac
Fabricio Pereira Rabelo <fabriciojbo@icloud.com>
2023-12-30 10:12:54 -0300

    changed jetstrem config

--- resources/views/profile/update-password-form.blade.php
+++ resources/views/profile/update-password-form.blade.php
@@ -29,7 +29,7 @@
 
     <x-slot name="actions">
         <x-action-message class="me-3" on="saved">
-            {{ __('lang.saved') }}
+            {{ __('lang.saved') }}.
         </x-action-message>
 
         <x-button>
